68a42109e14bf417087801a9e1871f104e2bb87e
Lukas Stucki <lusu@logobject.ch>
2024-04-10 15:18:02 +0200

    fix: rename file

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 # encoding: utf-8
-require_dependency 'redmine_jstoolbar_ext'
+require_relative 'lib/redmine_jstoolbar_ext_hook_listener'
 
 Redmine::Plugin.register :redmine_jstoolbar_ext do
   name 'Redmine jsToolbar Extension'
   author 'Thomas Leishman'
   description 'The Redmine JS Toolbar Extension add javascript functionality to add to the current jsToolbar.'
   version '0.2.1'
-  url 'https://github.com/tleish/redmine_jstoolbar_ext'
+  url 'https://github.com/stuckas/redmine_jstoolbar_ext'
   author_url 'https://github.com/tleish'
 end
